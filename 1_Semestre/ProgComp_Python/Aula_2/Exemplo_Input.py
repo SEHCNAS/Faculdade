9873fedffbc258bb1769e86e32f02e428b87e01f
Gabriel <thebug@outlook.com.br>
2022-03-09 09:30:12 -0300

    Metodos input e print

---
+++ 1_Semestre/ProgComp_Python/Aula_2/Exemplo_Input.py
@@ -0,0 +1,5 @@
+# Exemplo receber dados do usuario
+Nome = input('Digite o seu nome: ')
+Idade = input('Digite a sua idade: ')
+
+print(f'O sr(a) {Nome} \nTem {Idade} anos')
---
+++ 1_Semestre/ProgComp_Python/Aula_2/Exercicio_1.py
@@ -0,0 +1,9 @@
+# Exercicio de calculadora com dois nomes
+Numero_1 = float(input('Digite o primeiro numero: '))
+Numero_2 = float(input('Digite o segundo numero: '))
+
+print(f'A soma é {Numero_1 + Numero_2}')
+print(f'A sutração é {Numero_1 - Numero_2}')
+print(f'A multiplicação é {Numero_1 * Numero_2}')
+print(f'A divisão é {Numero_1 / Numero_2}')
+print(f'O resto de divisão é {Numero_1 % Numero_2}')
