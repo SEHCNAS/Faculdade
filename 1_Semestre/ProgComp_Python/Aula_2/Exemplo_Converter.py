325eb673666dd2f57ecbb12a52ba32a9b85bef23
Gabriel <thebug@outlook.com.br>
2022-03-09 09:29:19 -0300

    Como conventer os dados para outros tipos

---
+++ 1_Semestre/ProgComp_Python/Aula_2/Exemplo_Converter.py
@@ -0,0 +1,16 @@
+# Exemplo converter dados
+Nome = input('Digite o seu nome: ')
+Idade = int(input('Digite a sua idade: '))
+
+print(type(Nome))
+print(type(Idade))
+
+# Outros tipos de conversão
+
+"""
+
+float()
+Int()
+str()
+
+"""
