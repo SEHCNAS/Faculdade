f047e1d3ab68dd166e42ec9bc453faa0c21d81e9
Gabriel <thebug@outlook.com.br>
2022-03-10 11:35:53 -0300

    Adicionado try except

--- 1_Semestre/ProgComp_Python/Aula_2/Exercicios/Exercicio_1.py
+++ 1_Semestre/ProgComp_Python/Aula_2/Exercicios/Exercicio_1.py
@@ -1,14 +1,19 @@
 # Calculo area e perimetro do retangulo
 print('-- Calculo da base e perimetro de um retangulo --')
-AlturaRetangulo = float(input('Digite a altura do retangulo: '))
-BaseRetangulo = float(input('Digite a Base(Largura) do retangulo: '))
+try:
+    AlturaRetangulo = float(input('Digite a altura do retangulo: '))
+    BaseRetangulo = float(input('Digite a Base(Largura) do retangulo: '))
 
-if AlturaRetangulo == BaseRetangulo:
-    print('As informações não batem '
-          'com as caracteristica de um retangulo'
-          ' - A base não pode ser igual a altura')
+except ValueError as error:
+    print(f'Ocorreu o seguinte erro - {error}')
+
 else:
-    AreaRetangulo = AlturaRetangulo * BaseRetangulo
-    PerimetroRetangulo = 2*(BaseRetangulo + AlturaRetangulo)
-    print(f'A area do retangulo é {AreaRetangulo} e o'
-          f' perimetro é {PerimetroRetangulo}')
+    if AlturaRetangulo == BaseRetangulo:
+        print('As informações não batem '
+              'com as caracteristica de um retangulo'
+              ' - A base não pode ser igual a altura')
+    else:
+        AreaRetangulo = AlturaRetangulo * BaseRetangulo
+        PerimetroRetangulo = 2*(BaseRetangulo + AlturaRetangulo)
+        print(f'A area do retangulo é {AreaRetangulo} e o'
+              f' perimetro é {PerimetroRetangulo}')
--- 1_Semestre/ProgComp_Python/Aula_2/Exercicios/Exercicio_2.py
+++ 1_Semestre/ProgComp_Python/Aula_2/Exercicios/Exercicio_2.py
@@ -1,4 +1,10 @@
 # somar 5% ao salario informado
-Salario = float(input('Digite o salario: '))
-SalarioAcrescido = Salario * 1.05
-print(f'O salario acrescido de 5% é {SalarioAcrescido}')
+try:
+    Salario = float(input('Digite o salario: '))
+
+except ValueError as error:
+    print(f'Ocorreu o seguinte erro - {error}')
+
+else:
+    SalarioAcrescido = Salario * 1.05
+    print(f'O salario acrescido de 5% é {SalarioAcrescido}')
