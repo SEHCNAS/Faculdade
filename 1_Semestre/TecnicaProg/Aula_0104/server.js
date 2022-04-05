e2b3bfa55ef33790e68c4c50b039ee09c914482b
Gabriel <70602877+SEHCNAS@users.noreply.github.com>
2022-04-05 20:53:50 -0300

    Delete 1_Semestre/TecnicaProg directory

--- 1_Semestre/TecnicaProg/Aula_0104/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-//Define a porta 
-app.set('port', 7777);
-
-const server = app.listen(app.get('port'), () =>{
-    console.log("O servidor rodando na porta: " + server.address().port);
-});
