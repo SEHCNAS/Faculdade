cf1d978bf63e3de15b096af9eb206e58d3e87e76
Gabriel <thebug@outlook.com.br>
2023-03-16 14:49:23 -0300

    Exercicios

---
+++ 3_Semestre/TecnicasDeProgramacao/projeto-aula/src/Componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css';
+export default Banner;
+
+function Banner(){
+    return(
+      <header className='Banner'>
+          <img src = './imagens/Banner.png' alt=''></img>
+      </header>
+    );
+}
